92aef628fda8202c75f17f8c3d3cd798cd969746
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-18 11:44:02 -0300

    local storege

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,7 @@
+export const ListaProdutos = [
+    {id:1,nome:'MouseGamer',desc:'Mecanico', preco: 170},
+    {id:2,nome:'Teclado',desc:'Brilhante', preco: 270},
+    {id:3,nome:'CadeiraGamer',desc:'Top', preco: 1170},
+    {id:4,nome:'MousePad',desc:'Plastico', preco: 70},
+    {id:5,nome:'Phone',desc:'Gamer', preco: 1000},
+]
